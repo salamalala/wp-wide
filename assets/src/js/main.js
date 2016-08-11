7f00d8af83284557cf339e546195c3f7970cec8e
salamalala <hello@ninaregli.ch>
2016-08-11 15:26:31 +0200

    fix(browser): fix issues with firefox browser

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -67,7 +67,7 @@ var stickyHeaderElements = function(el, scrollPos) {
 var inputFocus = function() {
   $('.support__address').on("focus", "input", function() {
     $(this).addClass("input__focused");
-    $(this).parents('.support__address').find("label").addClass("support__label");
+    $(this).parents('.support__address-element').find("label").addClass("support__label");
   });
 }
 
